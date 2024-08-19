fcca7afd4a6e8b0d1e9a404cddff2b916533f800
MiloszKom <milosz.kom@onet.pl>
2024-08-19 12:40:22 +0200

    Saving before restructuring the site to be mobile first

--- client/src/components/LocationsMap.jsx
+++ client/src/components/LocationsMap.jsx
@@ -1,6 +1,29 @@
 import React from "react";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 import darkThemeStyles from "./mapStyles.js";
+
+let userLat;
+let userLng;
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      userLat = position.coords.latitude;
+      userLng = position.coords.longitude;
+    });
+  }
+};
+
+getLocation();
+
+// Define an SVG for the blue ball marker
+const blueBallSVG = `
+  <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="10" stroke="white" stroke-width="2" fill="blue" />
+  </svg>
+`;
+
+const blueBallIcon = "data:image/svg+xml;base64," + btoa(blueBallSVG);
 
 export default function LocationsMap() {
   return (
@@ -10,11 +33,19 @@ export default function LocationsMap() {
         keyboardShortcuts={false}
         disableDefaultUI={true}
         defaultZoom={13}
-        defaultCenter={{ lat: 51.0443583, lng: 16.8675189 }}
+        defaultCenter={{ lat: userLat, lng: userLng }}
         options={{
           styles: darkThemeStyles, // Apply dark mode styles
         }}
-      />
+      >
+        {/* Marker with a custom blue ball icon */}
+        <Marker
+          position={{ lat: userLat, lng: userLng }}
+          icon={{
+            url: blueBallIcon,
+          }}
+        />
+      </Map>
     </APIProvider>
   );
 }
--- client/src/components/Planner.jsx
+++ client/src/components/Planner.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 export default function Header() {
+  const handleSearchClick = () => {
+    console.log("yay");
+  };
+
   return (
     <div className="planner-form">
       <h2>Best Spot</h2>
       <div className="search-bar">
-        <div className="search-bar-place"></div>
-        <div className="search-bar-distance"></div>
-        <div className="search-bar-search"></div>
+        <input className="search-bar-city" placeholder="City" />
+        <input className="search-bar-place" placeholder="Place" />
+        <div className="search-bar-search" onClick={handleSearchClick}>
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
       </div>
     </div>
   );
