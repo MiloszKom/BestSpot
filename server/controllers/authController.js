559b01397e9ac596603835d5df4bfde7208658ad
MiloszKom <milosz.kom@onet.pl>
2025-03-01 09:35:12 +0100

    Attempt to fix cookie issue

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,12 +16,12 @@ const createSendToken = (user, statusCode, res, message) => {
 
   const cookieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() +
+        (process.env.JWT_COOKIE_EXPIRES_IN || 30) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "None",
-    domain: ".bestspot.app",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     path: "/",
   };
 
